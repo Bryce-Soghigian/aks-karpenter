0086d795f77eb31448e3c5a89e6ee31816f1ce42
Bryce Soghigian <bsoghigian@gmail.com>
2024-06-04 11:58:30 -0700

    fix: ci lint

--- test/suites/acr/suite_test.go
+++ test/suites/acr/suite_test.go
@@ -1,9 +1,24 @@
+/*
+Portions Copyright (c) Microsoft Corporation.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package acr
 
 import (
 	"fmt"
-	"testing"
 	"os"
+	"testing"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -29,13 +44,13 @@ func TestAcr(t *testing.T) {
 		env = azure.NewEnvironment(t)
 		acrName := os.Getenv("AZURE_ACR_NAME")
 		Expect(acrName).NotTo(BeEmpty(), "AZURE_ACR_NAME must be set for the acr test suite")
-		pauseImage = fmt.Sprintf("%s.azurecr.io/pause:3.6",acrName)
+		pauseImage = fmt.Sprintf("%s.azurecr.io/pause:3.6", acrName)
 	})
 	RunSpecs(t, "Acr")
 }
 
-var _ = BeforeEach(func() { 
-	env.BeforeEach() 
+var _ = BeforeEach(func() {
+	env.BeforeEach()
 	nodeClass = env.DefaultAKSNodeClass()
 	nodePool = env.DefaultNodePool(nodeClass)
 })
@@ -43,8 +58,8 @@ var _ = AfterEach(func() { env.Cleanup() })
 var _ = AfterEach(func() { env.AfterEach() })
 
 var _ = Describe("Acr", func() {
-	Describe("Image Pull", func(){
-		It("should allow karpenter user pool nodes to pull images from the clusters attached acr", func(){
+	Describe("Image Pull", func() {
+		It("should allow karpenter user pool nodes to pull images from the clusters attached acr", func() {
 			deployment := test.Deployment(test.DeploymentOptions{
 				Replicas: 10,
 				PodOptions: test.PodOptions{
@@ -53,10 +68,10 @@ var _ = Describe("Acr", func() {
 							v1.ResourceCPU: resource.MustParse("1.1"),
 						},
 					},
-					Image: pauseImage, 
+					Image: pauseImage,
 				},
 			})
-			
+
 			env.ExpectCreated(nodePool, nodeClass, deployment)
 			env.EventuallyExpectHealthyPodCountWithTimeout(time.Minute*15, labels.SelectorFromSet(deployment.Spec.Selector.MatchLabels), int(*deployment.Spec.Replicas))
 		})
